8aae7525cb53a29699150f4833d6a2386fca0130
Isaac <olgatabora8@gmail.com>
2026-08-06 14:41:53 -0600

    Archivos De Tailwindcss

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>crud de tareas</title>
+    <link rel="stylesheet" href="assets/css/src/output.css">
 </head>
 <body>
+    <h1 class="uppercase  text-2xl bg-amber-600 font-bold text-center py-4">control de tareas en PHP</h1>
     
 </body>
 </html>
